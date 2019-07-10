353adab9174b82fec8b75e96253e6799564e2674
altcatalin <catalin.piscureanu@gmail.com>
2019-07-10 18:54:45 +0300

    aws deployment
    add cloudformation template

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
     "live_updates": true,
     "api": {
-        "endpoint": process.env.API_URL || "http://api.lvh.me:8080"
+        "endpoint": process.env.REACT_APP_API_URL || "http://api.lvh.me:8080"
     },
     "csrf_cookie": "nicecream_history_csrf"
 };
